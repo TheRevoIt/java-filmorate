c5cc875bee70129caceeddfe33f3ac3a32a06bb2
r3voit <revosierra@gmail.com>
2022-07-08 00:39:39 +0300

    fix:Прохождение тестов на GitHub-5

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTest.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = FilmorateApplication.class)
 @SpringBootTest
 class FilmorateApplicationTests {
 
